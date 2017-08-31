00c073b806d796bac00ad2c82c65c3f366b8cd1b
Danielle Gellis <danisyellis@gmail.com>
2017-08-31 11:01:08 -0700

    start db

--- src/models/db/db.js
+++ src/models/db/db.js
@@ -1,1 +1,14 @@
-const pgp = require(pg-promise);
+const pgp = require('pg-promise');
+const connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/todo_list';
+const db = pgp(connectionString);
+
+//check to see if we can connect to the db. If not, catch an error
+db.connect()
+    .then(obj => {
+        obj.done(); // success, release the connection;
+    })
+    .catch(error => {
+        console.log('ERROR:', error.message || error);
+    });
+
+module.exports = db;
